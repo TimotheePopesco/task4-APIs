fcf848fc990d5a1ced1e43410611e05bae3dadbb
Timothée  Popesco <timothee.popesco@edu.devinci.fr>
2022-10-19 14:22:55 +0100

    finish the initialisation of the files

---
+++ apis/lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:apis/screens/homepage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task4-APIs-HTTPrequest',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Task4-APIs-HTTPrequest Homepage'),
+    );
+  }
+}
